3eba0053add50fa193f54f3b69fe4fca9cfcdac9
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2023-04-17 07:17:02 +0300

    Fix field accessibility for Quote class

--- quotes/src/main/java/com/yuriisurzhykov/goaldestinator/quotes/data/Quote.kt
+++ quotes/src/main/java/com/yuriisurzhykov/goaldestinator/quotes/data/Quote.kt
@@ -6,16 +6,16 @@ interface Quote {
 
     data class Base(
         @SerializedName("id")
-        val id: String,
+        private val id: String,
         @SerializedName("content")
-        val content: String,
+        private val content: String,
         @SerializedName("author")
-        val author: String,
+        private val author: String,
         @SerializedName("authorSlug")
-        val authorSlug: String,
+        private val authorSlug: String,
         @SerializedName("length")
-        val length: Long,
+        private val length: Long,
         @SerializedName("tags")
-        val tags: List<String>
+        private val tags: List<String>
     ) : Quote
 }
