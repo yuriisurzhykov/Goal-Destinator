4a06c65009d7654e46c8ab9edb8631a9876ed3ed
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2024-07-25 17:22:16 -0700

    Started tasks screen
    Signed-off-by: Yurii Surzhykov <yuriisurzhykov@gmail.com>

--- goals-creation/src/main/kotlin/com/yuriisurzhykov/goaldestinator/goals/creation/CreateTasksScreen.kt
+++ goals-creation/src/main/kotlin/com/yuriisurzhykov/goaldestinator/goals/creation/CreateTasksScreen.kt
@@ -1,2 +1,14 @@
 package com.yuriisurzhykov.goaldestinator.goals.creation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CreateTasksScreen(
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier,
+    onNextClicked: (List<String>) -> Unit
+) {
+
+}
